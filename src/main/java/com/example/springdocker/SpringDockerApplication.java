e44381ff9576fe1ca6b5c8e311a3d334edb3b223
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-11-16 14:27:07 -0800

    add rest controller for home

--- src/main/java/com/example/springdocker/SpringDockerApplication.java
+++ src/main/java/com/example/springdocker/SpringDockerApplication.java
@@ -2,9 +2,17 @@ package com.example.springdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDockerApplication {
+
+	@GetMapping
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerApplication.class, args);
